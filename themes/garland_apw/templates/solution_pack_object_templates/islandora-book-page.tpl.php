f82f707494cee9cb9fcc483044b96f63eb1d09f7
Pat Dunlavey <patrick.dunlavey@commonmediainc.com>
2017-07-17 17:16:06 -0400

    pat / dev - get book page theming on the webform/transcription train.
    Why:
    
    * No transcription link displayed on book pages.
    
    This change addresses the need by:
    
    * Modifying the islandora-book-page template to be webform-(and transcription)-aware.
    
    Reference Links:
    
    * https://commonmedia.teamworkpm.net/#tasks/12888592

--- themes/garland_apw/templates/solution_pack_object_templates/islandora-book-page.tpl.php
+++ themes/garland_apw/templates/solution_pack_object_templates/islandora-book-page.tpl.php
@@ -6,22 +6,39 @@
 ?>
 <!-- Begin sites/all/themes/garland_apw/templates/solution_pack_object_templates/islandora-book-page.tpl.php -->
 <?php
-  print $book_object_id ? l(t('Return to Book View'), "islandora/object/{$book_object_id}") : t('Orphaned page (no associated book)');
+print $book_object_id ? l(t('Return to Book View'), "islandora/object/{$book_object_id}") : t('Orphaned page (no associated book)');
 ?>
-<?php if (isset($viewer)): ?>
-  <div id="book-page-viewer">
-    <?php print $viewer; ?>
-  </div>
-<?php elseif (isset($object['JPG']) && islandora_datastream_access(ISLANDORA_VIEW_OBJECTS, $object['JPG'])): ?>
-  <div id="book-page-image">
-    <?php
-      $params = array(
-        'path' => url("islandora/object/{$object->id}/datastream/JPG/view"),
-        'attributes' => array(),
-      );
-      print theme('image', $params);
-    ?>
-  </div>
+<?php if (!empty($webform_links)) : ?>
+  <?php print render($webform_links); ?>
 <?php endif; ?>
+<div id="islandora_object_display_wrapper" class="clearfix">
+  <div class="book-page-content-wrapper islandora-object-content-wrapper clearfix">
+
+    <?php if (isset($viewer)): ?>
+      <div id="book-page-viewer">
+        <?php print $viewer; ?>
+      </div>
+    <?php elseif (isset($object['JPG']) && islandora_datastream_access(ISLANDORA_VIEW_OBJECTS, $object['JPG'])): ?>
+      <div id="book-page-image">
+        <?php
+        $params = array(
+            'path' => url("islandora/object/{$object->id}/datastream/JPG/view"),
+            'attributes' => array(),
+        );
+        print theme('image', $params);
+        ?>
+      </div>
+    <?php endif; ?>
+
+  </div>
+
+  <?php if ($webform_target) : ?>
+    <div id="apwa_inline_webform_target_wrapper">
+      <?php print(render($webform_target)); ?>
+    </div>
+  <?php endif; ?>
+
+</div>
+
 <!-- @todo Add table of metadata values -->
 <!-- End sites/all/themes/garland_apw/templates/solution_pack_object_templates/islandora-book-page.tpl.php -->
